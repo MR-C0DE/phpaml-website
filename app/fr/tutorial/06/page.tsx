11e158f8979d0de96ed4bf528ad5336678c9228e
MR-C0DE <80490263+MR-C0DE@users.noreply.github.com>
2026-08-22 04:49:08 -0400

    docs: publish comprehensive views chapter

---
+++ app/fr/tutorial/06/page.tsx
@@ -0,0 +1,2 @@
+import { ChapterSix } from "../../../tutorial/chapter-six";
+export default function Page(){ return <ChapterSix locale="fr"/>; }
---
+++ app/tutorial/06/page.tsx
@@ -0,0 +1,2 @@
+import { ChapterSix } from "../chapter-six";
+export default function Page(){ return <ChapterSix locale="en"/>; }
